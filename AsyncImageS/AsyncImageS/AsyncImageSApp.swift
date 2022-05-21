1dc24726871860232649b82a94caede28f7e4748
gurjindersingh <gurjindersingh663@gmail.com>
2022-05-21 22:47:42 +0530

    initial commit

---
+++ AsyncImageS/AsyncImageS/AsyncImageSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSApp.swift
+//  AsyncImageS
+//
+//  Created by Gurjinder Singh on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
